9b4c324a63179d9ef28a419e1d28d9b306543e9e
AlexGlering <afglering@gmail.com>
2023-10-26 00:03:20 +0100

    setup

---
+++ InfoSwipe.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import Swiper from "react-native-swiper";
+
+export function InfoSwipe() {
+  const slidesData = [
+    {
+      image: require("./assets/luxury1.jpg"),
+      title: "Boost Your Reputation With Amazing Cars",
+      descriptor:
+        "Rent amazing cars from all around, and display them on your social platforms.",
+    },
+    {
+      image: require("./assets/luxury2.jpg"), // Change to your actual path
+      title: "Be A Super Car Owner, Without Breaking The Bank",
+      descriptor:
+        "Owning a super car has never been easier. Spend your money and take our cars.",
+    },
+    {
+      image: require("./assets/luxury3.jpg"), // Change to your actual path
+      title: "Have A Super Car That's Not Being Used? Rent It Out!",
+      descriptor:
+        "Rent out your super car to others and make money while you're not using it.",
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <Swiper showsButtons={false} paginationStyle={styles.paginationStyle}>
+        {slidesData.map((slide, index) => (
+          <View key={index} style={styles.slide}>
+            <Image source={slide.image} style={styles.logo} />
+            <View style={styles.loginCard}>
+              <Text style={styles.title}>{slide.title}</Text>
+              <Text style={styles.paragraph}>{slide.descriptor}</Text>
+              <TouchableOpacity
+                style={styles.continueButton}
+                onPress={() => {
+                  /* Continue button action here */
+                }}
+              >
+                <Text style={styles.buttonText}>Continue</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.skipButton}
+                onPress={() => {
+                  /* Skip button action here */
+                }}
+              >
+                <Text style={styles.buttonText2}>Skip</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        ))}
+      </Swiper>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  slide: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  logo: {
+    height: "60%", // Adjusting the height
+    width: "100%",
+    resizeMode: "cover",
+  },
+  loginCard: {
+    flex: 1,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 20,
+    backgroundColor: "#fff",
+    shadowOffset: { width: 0, height: -3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5,
+    marginTop: -30,
+    maxHeight: "60%", // Setting a maximum height
+  },
+  title: {
+    fontSize: 24,
+    alignSelf: "center",
+    textAlign: "center",
+    marginTop: 20,
+    fontWeight: "bold",
+  },
+  paragraph: {
+    marginVertical: 10,
+    fontSize: 16,
+    color: "#000",
+    textAlign: "center",
+    opacity: 0.5,
+  },
+  continueButton: {
+    backgroundColor: "#0E5E28",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+    marginVertical: 5,
+  },
+  skipButton: {
+    backgroundColor: "#DBDBDB",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+    marginVertical: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  buttonText2: {
+    color: "#000",
+    textAlign: "center",
+  },
+  paginationStyle: {
+    bottom: "40%", // Adjust this value to position the dots as desired above the card header
+  },
+});
---
+++ LoginScreen.tsx
@@ -0,0 +1,131 @@
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+export function LoginScreen() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./assets/luxury.jpg")} style={styles.logo} />
+
+      <View style={styles.loginCard}>
+        <Text style={styles.title}>Sign In</Text>
+        <TextInput placeholder="Username" style={styles.input} />
+        <TextInput
+          placeholder="Password"
+          style={styles.input}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            /* Button action here */
+          }}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <View style={styles.bottomTextContainer}>
+          <TouchableOpacity
+            onPress={() => {
+              /* Button action here */
+            }}
+          >
+            <Text>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              /* Button action here */
+            }}
+          >
+            <Text>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.bottomText}>OR</Text>
+        <TouchableOpacity
+          style={styles.button2}
+          onPress={() => {
+            /* Button action here */
+          }}
+        >
+          <Text style={styles.buttonText2}>Sign in with instagram</Text>
+        </TouchableOpacity>
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  logo: {
+    height: "50%",
+    width: "100%",
+    resizeMode: "cover",
+  },
+  loginCard: {
+    flex: 1,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 20,
+    backgroundColor: "#fff",
+    shadowOffset: { width: 0, height: -3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    elevation: 5, // For Android
+    marginTop: -30,
+  },
+  button: {
+    backgroundColor: "#0E5E28",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+    marginVertical: 5,
+  },
+  button2: {
+    backgroundColor: "#DBDBDB",
+    padding: 10,
+    borderRadius: 5,
+    width: "100%",
+    marginVertical: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+  buttonText2: {
+    color: "#000",
+    textAlign: "center",
+  },
+  bottomTextContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  bottomText: {
+    color: "#000",
+    textAlign: "center",
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+    alignSelf: "center",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "gray",
+    padding: 10,
+    marginVertical: 5,
+    borderRadius: 5,
+    width: "100%",
+  },
+});
