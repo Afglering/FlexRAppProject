5a24bbe936f2d6244bcc3c4d32211e29825acb94
AlexGlering <afglering@gmail.com>
2023-10-30 21:22:45 +0000

    Booking: Alert on success alert on fail

--- screens/Browse.tsx
+++ screens/Browse.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, Image, FlatList, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, Image, FlatList, TouchableOpacity, Alert } from "react-native";
 import { getFirestore, collection, getDocs, arrayUnion, doc, updateDoc } from "firebase/firestore"; // Firestore imports
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../App";
@@ -34,27 +34,64 @@ const Browse: React.FC<BrowseScreenProps> = ({ route }) => {
   const handleBookNow = async (selectedCar: Car) => {
     try {
       const userId = getAuth().currentUser?.uid;
-
+  
       if (!userId) {
         console.error("User is not signed in");
         return;
       }
-
+  
+      // Check if the car is already booked
+      if (!selectedCar.isAvailable) {
+        // Alert for car already booked
+        Alert.alert(
+          "Booking Unavailable",
+          `The ${selectedCar.make} ${selectedCar.model} is already booked.`,
+          [
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ],
+          { cancelable: false }
+        );
+        return; // Exit the function early
+      }
+  
       const userDocRef = doc(db, "users", userId);
       await updateDoc(userDocRef, {
         cars_Rented: arrayUnion(selectedCar.id)
       });
-
+  
       const carDocRef = doc(db, "cars", selectedCar.id);
       await updateDoc(carDocRef, {
         isAvailable: false
       });
-
+  
       fetchCars();
+  
+      // Alert for successful booking
+      Alert.alert(
+        "Booking Successful",
+        `You've successfully booked the ${selectedCar.make} ${selectedCar.model}!`,
+        [
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ],
+        { cancelable: false }
+      );
+      
     } catch (error) {
       console.error("Error booking car:", error);
+  
+      // Alert for unsuccessful booking
+      Alert.alert(
+        "Booking Failed",
+        "An error occurred while trying to book the car. Please try again.",
+        [
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ],
+        { cancelable: false }
+      );
     }
   };
+  
+  
 
   const renderCarCard = (car: Car) => (
     <TouchableOpacity style={styles.card} key={car.id}>
